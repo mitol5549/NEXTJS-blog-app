674b9cd11fafffd6664b3bc9e65916a40c42b791
Andrey Osipov <mitol5549@gmail.com>
2023-09-24 13:27:16 +0200

    second Post as Markdown added

--- components/posts/post-detail/PostContent.jsx
+++ components/posts/post-detail/PostContent.jsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 import { PostHeader } from './PostHeader';
 
@@ -47,7 +49,7 @@ export const PostContent = props => {
       const { className, children } = code;
 
       const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
-      
+
       return <SyntaxHighlighter style={atomDark} language={language} children={children} />;
     },
   };
