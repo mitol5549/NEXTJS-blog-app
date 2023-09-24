2852109a2dad0435221c8951c0d1833405eb16a5
Andrey Osipov <mitol5549@gmail.com>
2023-09-24 12:24:01 +0200

    rendering Dynamic Page

--- helpers/posts-util.js
+++ helpers/posts-util.js
@@ -5,14 +5,18 @@ import matter from 'gray-matter';
 
 const postsDirectory = path.join(process.cwd(), 'posts');
 
-const getPostData = fileName => {
-  const filePath = path.join(postsDirectory, fileName);
+export const getPostsFiles = () => {
+  return fs.readdirSync(postsDirectory);
+};
+
+export const getPostData = postIdentifier => {
+  const postSlug = postIdentifier.replace(/\.md$/, '');
+
+  const filePath = path.join(postsDirectory, `${postSlug}.md`);
 
   const fileContent = fs.readFileSync(filePath, 'utf-8');
 
   const { data, content } = matter(fileContent);
-
-  const postSlug = fileName.replace(/\.md$/, '');
 
   const postData = {
     slug: postSlug,
@@ -24,7 +28,7 @@ const getPostData = fileName => {
 };
 
 export const getAllPosts = () => {
-  const postFiles = fs.readdirSync(postsDirectory);
+  const postFiles = getPostsFiles();
 
   const allPosts = postFiles.map(postFile => {
     return getPostData(postFile);
--- pages/posts/[slug].jsx
+++ pages/posts/[slug].jsx
@@ -1,5 +1,31 @@
 import { PostContent } from '../../components/posts/post-detail/PostContent';
+import { getPostData, getPostsFiles } from '../../helpers/posts-util';
 
-export default function PostDetailPage() {
-  return <PostContent />;
+export default function PostDetailPage(props) {
+  return <PostContent post={props.post} />;
+}
+
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
+}
+
+export function getStaticPaths() {
+  const postFileName = getPostsFiles();
+
+  const slugs = postFileName.map(fileName => fileName.replace(/\.md$/, ''));
+
+  return {
+    paths: slugs.map(slug => ({ params: { slug } })),
+    fallback: false,
+  };
 }
