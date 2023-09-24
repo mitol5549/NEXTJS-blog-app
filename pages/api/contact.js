8aac288025726f931481812ef9c090898cdf402c
Andrey Osipov <mitol5549@gmail.com>
2023-09-24 16:24:34 +0200

    MongoDB connected

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { email, name, message } = req.body;
 
@@ -15,7 +17,30 @@ export default function handler(req, res) {
       message,
     };
 
-    console.log(newMessage);
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        'mongodb+srv://mitol:0kQ6UJVhdtT1c8U8@eventsapp.suim1u3.mongodb.net/?retryWrites=true&w=majority',
+      );
+    } catch (error) {
+      res.status(500).json({ message: 'Could not connect to database.' });
+      return;
+    }
+
+    const db = client.db('blog');
+
+    try {
+      const result = await db.collection('messages').insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+
+      res.status(500).json({ message: 'Storing message failed!' });
+      return;
+    }
+
+    client.close();
 
     res.status(201).json({ message: 'Successfully stored message!', message: newMessage });
   }
